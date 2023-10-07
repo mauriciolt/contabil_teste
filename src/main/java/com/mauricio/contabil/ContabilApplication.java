15903ed6b5c93c20c7fe49665b55ed0cb050a7d3
Mauricio Teixeira <mauriciolt@gmail.com>
2023-10-07 14:48:44 -0300

    Projeto Contabil Iniciado

---
+++ src/main/java/com/mauricio/contabil/ContabilApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.contabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilApplication.class, args);
+	}
+
+}
